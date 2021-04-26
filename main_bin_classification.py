ee7b76fb6aebc1041e334189e0305530fffd73d4
OJASV Kamal <43926001+kamalojasv181@users.noreply.github.com>
2021-04-26 09:19:52 +0530

    Update main_bin_classification.py

--- main_bin_classification.py
+++ main_bin_classification.py
@@ -1,11 +1,22 @@
 from data_processing import preprocessing
+import numpy as np
 from binary_classification import CustomDataset, ModelClass, bin_classifier
 
-data = preprocessing('./Dataset/constraint_Hindi_Train - Sheet1_combined.csv')
+data = preprocessing('./Dataset/constraint_Hindi_Train - Sheet1.csv')
 arr = data.processed()
 
+data_valid = preprocessing('./Dataset/Constraint_Hindi_Valid - Sheet1.csv')
+arr_valid = data_valid.processed()
 
-mod = bin_classifier(arr, "mrm8488/HindiBERTa", 'non-hostile', 10, 1e-5)
+arr = arr.tolist()
+arr_valid = arr_valid.tolist()
+
+for row in arr_valid:
+	arr.append(row)
+arr = np.array(arr)
+arr_valid = np.array(arr_valid)
+
+mod = bin_classifier(arr, "ai4bharat/indic-bert", 'non-hostile', 10, 1e-5)
 mod.train_model()
 
 
