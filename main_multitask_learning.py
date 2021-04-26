c33128bd63cbd38b2b08537a483869a0eac2bacd
OJASV Kamal <43926001+kamalojasv181@users.noreply.github.com>
2021-04-26 09:20:14 +0530

    Update main_multitask_learning.py

--- main_multitask_learning.py
+++ main_multitask_learning.py
@@ -1,5 +1,20 @@
 from data_processing import preprocessing
 from multitask_learning import CustomDataset, ModelClass, multitask_classifier
+import numpy as np
 
-data = preprocessing('./Dataset/constraint_Hindi_Train - Sheet1_combined.csv')
+data = preprocessing('./Dataset/constraint_Hindi_Train - Sheet1.csv')
 arr = data.processed()
+
+data_valid = preprocessing('./Dataset/Constraint_Hindi_Valid - Sheet1.csv')
+arr_valid = data_valid.processed()
+
+arr = arr.tolist()
+arr_valid = arr_valid.tolist()
+
+for row in arr_valid:
+	arr.append(row)
+arr = np.array(arr)
+arr_valid = np.array(arr_valid)
+
+mod = multitask_classifier(arr, "ai4bharat/indic-bert", 'non-hostile', 20, 1e-5)
+mod.train_model()
